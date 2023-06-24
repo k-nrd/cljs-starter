4eabf7e49b1a011e91c57baad6b93de01711712c
Gustavo Konrad <g.konrad@outlook.com>
2023-06-24 17:43:29 +0200

    added malli specs

--- src/app_name/core.cljs
+++ src/app_name/core.cljs
@@ -1,13 +1,17 @@
-(ns app-name.core)
+(ns app-name.core
+  (:require [malli.core :as m]))
 
+(m/=> start [:=> :cat :nil])
 (defn start []
   (js/console.log "start"))
 
+(m/=> stop [:=> :cat :nil])
 (defn stop []
   ;; stop is called before any code is reloaded
   ;; this is controlled by :before-load in the config
   (js/console.log "stop"))
 
+(m/=> init [:=> :cat :nil])
 (defn init []
   ;; init is called ONCE when the page loads
   ;; this is called in the index.html and must be exported
